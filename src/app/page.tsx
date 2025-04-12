f6f16bbff7eea883bc8868aea0996821181e5517
Mohammad <johndoe@example.com>
2025-04-12 02:18:48 -0700

    crating sticky contact button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Gallery from "@/components/Gallery";
 import Hero from "@/components/Hero";
 import SwipperProductCards from "@/components/SwipperProductCards";
 import Video from "@/components/Video";
+import StickyCallIcon from "@/components/StickyCallIcon";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <About />
       <SwipperProductCards />
       <Gallery />
+      <StickyCallIcon />
     </>
   );
 }
---
+++ src/components/StickyCallIcon.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Calling from "../../public/Calling.gif";
+import ModalPage from "./ModalPage";
+import { useState } from "react";
+
+export default function StickyCallIcon() {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false); // Controls the modal for contact
+
+  return (
+    <div className="relative">
+      <button
+        onClick={() => setIsModalOpen(true)} // Open modal on button click
+        className="h-12 w-12 rounded-full border border-white bg-white/10 fixed bottom-7 right-7 flex justify-center items-center z-50 lg:hidden cursor-pointer"
+      >
+        <Image
+          src={Calling}
+          alt="Calling"
+          width={18}
+          height={18}
+          className="w-12 h-12 object-contain p-1"
+          unoptimized
+        />
+      </button>
+      {/* Modal for Calling */}
+      <ModalPage isOpen={isModalOpen} setIsOpen={setIsModalOpen} />
+    </div>
+  );
+}
