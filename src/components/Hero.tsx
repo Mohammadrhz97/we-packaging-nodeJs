d829490c32e6a277ecb268ccf91ab44c77b208e9
Mohammad <johndoe@example.com>
2025-04-26 02:07:32 -0700

    adjusting hero image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,6 +7,8 @@ import { FiExternalLink } from "react-icons/fi";
 import Image from "next/image";
 
 import P1 from "../../public/1.jpg"; // Desktop image
+import P2 from "../../public/2.jpg"; // Desktop image
+import P4 from "../../public/4.jpg"; // Desktop image
 
 const Hero: React.FC = () => {
   return (
@@ -20,17 +22,29 @@ const Hero: React.FC = () => {
       {/* Content Layer */}
       <div className="relative z-10 w-full h-full flex justify-between items-center flex-col pt-[10px]">
         {/* Blog post image */}
-        <div className="sm:hidden w-full h-auto">
-          <Image
-            src={P1}
-            alt="Hero Page"
-            height={620}
-            width={620}
-            className="object-cover absolute top-14 left-0 "
-          />
+        <div className="sm:hidden ">
+          <div className="mt-10">
+            <Image src={P1} alt="Hero image1" height={620} width={620} />
+          </div>
+          <div>
+            <Image src={P2} alt="Hero image2" height={620} width={620} />
+          </div>
+          <div className="relative">
+            <Image src={P4} alt="Hero image2" height={620} width={620} />
+            <Link href="/products">
+              <div className="absolute sm:hidden w-full top-10 backdrop-blur-xs h-16 flex justify-center items-center">
+                <button className="backdrop-blur-xs flex items-center h-11 justify-center text-white text-xl gap-4 border border-black border-opacity-60 border-r-3 border-b-3 p-1 px-3 z-30 cursor-pointer">
+                  <span>
+                    <FiExternalLink className="w-4 h-4 text-white" />
+                  </span>
+                  برای دیدن لیست محصولات کلیک کنید
+                </button>
+              </div>
+            </Link>
+          </div>
         </div>
         {/* Typing animation text */}
-        <div className="absolute z-20  w-full h-[140px] md:h-[220px] backdrop-blur-xs top-[420px] sm:top-12">
+        <div className="absolute z-20  w-full h-[140px] md:h-[220px] backdrop-blur-xs top-[420px] sm:top-12 hidden sm:block">
           <TypeAnimation
             style={{
               whiteSpace: "pre-line",
@@ -47,7 +61,7 @@ const Hero: React.FC = () => {
 
         {/* Button */}
         <Link href="/products">
-          <div className="absolute w-full backdrop-blur-xs h-16 sm:bottom-16 bottom-42 right-0 flex justify-center items-center">
+          <div className="absolute hidden w-full backdrop-blur-xs h-16 sm:bottom-16 bottom-42 right-0 sm:flex justify-center items-center">
             <button className="backdrop-blur-xs flex items-center h-11 justify-center text-white text-xl gap-4 border border-black border-opacity-60 border-r-3 border-b-3 p-1 px-3 z-30 cursor-pointer">
               <span>
                 <FiExternalLink className="w-4 h-4 text-white" />
