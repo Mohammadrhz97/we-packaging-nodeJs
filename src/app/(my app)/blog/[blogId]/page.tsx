17e2c1975e54667e10e862cb34e5fc6558e689c5
Mohammad <johndoe@example.com>
2025-05-19 05:32:30 -0700

    generating metadata for blogs

--- src/app/(my app)/blog/[blogId]/page.tsx
+++ src/app/(my app)/blog/[blogId]/page.tsx
@@ -17,6 +17,22 @@ export async function generateMetadata({
   return {
     title: product?.title || "Product Not Found", // Fallback title
     description: product?.excerpt, // Optional meta description
+    openGraph: {
+      title: `${product?.title}`,
+      description:
+        "به وی پکیجینگ خوش آمدید! ما ارائه‌دهنده بهترین محصولات و خدمات در زمینه توسعه برند و پکیجینگ هستیم. با خرید از ما، کیفیت و رضایت را تجربه کنید. از مجموعه متنوع محصولات و پیشنهادات ویژه‌مان بازدید کنید.",
+      url: "/blog",
+      siteName: "وی پکیجینگ",
+      images: [
+        {
+          url: `/${product?.image}.jpg`,
+          width: 1200,
+          height: 630,
+        },
+      ],
+      locale: "fa_IR",
+      type: "website",
+    },
   };
 }
 
